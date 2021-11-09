85e9a0abab38ba2ddc01da05318c30f3a3326fad
Oidoioi1 <93958419+Oidoioi1@users.noreply.github.com>
2021-11-09 15:27:06 +0700

    Add files via upload

---
+++ 2kl;2kl.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n,x,y; 
+	printf("Nhap gia tri n :") ;
+	scanf("%d",&n);
+	x=y=n;
+	if (n%2 == 0 && x>3)
+	
+	x = 0;
+	
+	else 
+
+		y=0; 
+	 
+	printf("Gia tri cua x y la: %d và %d",x,y);
+}
